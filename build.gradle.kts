c5263a1717986a173b0b2086112d15ec3825874a
GokulnathP <4gokul2000@gmail.com>
2024-05-04 11:13:46 +0530

    [Gokul] Create tasks and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,3 +11,18 @@ repositories { mavenCentral() }
 dependencies { testImplementation(libs.junit.jupiter) }
 
 tasks.named<Test>("test") { useJUnitPlatform() }
+
+tasks.register("hello") {
+    group = "Greet"
+
+    doFirst { println("First") }
+
+}
+
+tasks.register("greet") {
+    group = "Greet"
+
+    doLast { exec { commandLine("java", "--version") } }
+
+    dependsOn("hello")
+}
